9d395747ce0b300cc010c06f3fcd3aae4fa8df0f
Sereban4o <106669883+Sereban4o@users.noreply.github.com>
2023-03-15 11:15:54 +0300

    HW1

--- HW1/number_3.php
+++ HW1/number_3.php
@@ -7,7 +7,9 @@ for ($i = 1; $i <= 3; $i++) {
 }
 
 echo "$name, сегодня у вас запланировано 3 приоритетных задачи на день:" . PHP_EOL;
-
+$allTime = 0;
 for ($i = 1; $i <= 3; $i++) {
-    echo "-${"task$i"} (${"time$i"})" . PHP_EOL;
-}
+    echo "-${"task$i"} (${"time$i"})ч" . PHP_EOL;
+    $allTime = $allTime + ${"time$i"}; 
+};
+echo "Примерное время выполнения плана = $allTime ч";
