da5d535e5696810b6e5cb2ad822ee803df463728
Sereban4o <106669883+Sereban4o@users.noreply.github.com>
2023-03-15 15:49:40 +0300

    HW1

---
+++ HW1/number_3_2.php
@@ -0,0 +1,26 @@
+<?php
+define("STR_QUESTION", "Какая задача стоит перед вами сегодня?");
+define("STR_TIME", "Сколько примерно времени эта задача займет?");
+$tasks = 3;
+
+$name = readline("Напишите Ваше имя.");
+$age = (int)readline("Сколько Вам лет?");
+
+$task1 = readline(STR_QUESTION);
+$time1 = (int)readline(STR_TIME);
+
+$task2 = readline(STR_QUESTION);
+$time2 = (int)readline(STR_TIME);
+
+$task3 = readline(STR_QUESTION);
+$time3 = (int)readline(STR_TIME);
+
+$allTime = $time1 + $time2 + $time3;
+
+echo "Вас зовут $name, вам $age лет"  . PHP_EOL;
+echo "$name, сегодня у вас запланировано $tasks приоритетных задачи на день:" . PHP_EOL;
+echo "- $task1 ($time1)ч" . PHP_EOL;
+echo "- $task2 ($time2)ч" . PHP_EOL;
+echo "- $task3 ($time3)ч" . PHP_EOL;
+
+echo "Примерное время выполнения плана =" . $allTime . "ч";
