e6f03dc253ba62b0d822cbfca42a551c1c665955
Sereban4o <106669883+Sereban4o@users.noreply.github.com>
2023-04-05 12:20:02 +0300

    Revert "HW6"
    This reverts commit 33d36e1d3b608bc853c8360b34cb606b662647a0.

--- HW6/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-// routes.php
-return [
-    'home' => 'controller/HomeController.php',
-    'security' => 'controller/SecurityController.php',
-    'tasks' => 'controller/TasksController.php'
-];
