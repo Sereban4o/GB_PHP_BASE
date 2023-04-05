33d36e1d3b608bc853c8360b34cb606b662647a0
Sereban4o <106669883+Sereban4o@users.noreply.github.com>
2023-04-05 12:16:48 +0300

    HW6

---
+++ HW6/controller/SecurityController.php
@@ -0,0 +1,29 @@
+<?php
+// controller/SecurityController.php
+
+require_once 'model/UserProvider.php';
+session_start();
+
+$error = null;
+if (isset($_POST['username'], $_POST['password'])) {
+    ['username' => $username, 'password' => $password] = $_POST;
+    $userProvider = new UserProvider();
+    $user = $userProvider->getByUsernameAndPassword($username, $password);
+    if ($user === null) {
+        $error = 'Пользователь с указанными учетными данными не найден';
+    } else {
+        $_SESSION['user'] = $user;
+        header("Location: index.php");
+        die();
+    }
+}
+
+
+if (isset($_GET['action']) && $_GET['action'] === 'logout') {
+    unset($_SESSION['user']);
+    unset($_SESSION['tasks']);
+    header("Location: index.php");
+    die();
+}
+
+require_once 'view/signin.php';
